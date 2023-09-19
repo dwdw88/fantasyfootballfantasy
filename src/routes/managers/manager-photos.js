5b35eea35e85d7fc29e7076e05b8d0585fc6ffc8
dwdw88 <145488095+dwdw88@users.noreply.github.com>
2023-09-19 18:21:07 -0400

    Create manager-photos.js

---
+++ src/routes/managers/manager-photos.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Define a route for manager photos
+app.get('/manager-photos/:managerName', (req, res) => {
+  const managerName = req.params.managerName;
+  const photoPath = path.join(__dirname, 'static/managers', `${managerName}.png`);
+
+  // Check if the photo file exists
+  // If it exists, send the photo as a response
+  // If it doesn't exist, you can send a default image or an error message
+  res.sendFile(photoPath, (err) => {
+    if (err) {
+      res.status(404).send('Photo not found');
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
